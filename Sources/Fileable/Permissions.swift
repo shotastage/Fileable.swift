b0e7984f2b14d49469fa128cae12bf365e923715
Shota Shimazu <hornet.live.mf@gmail.com>
2018-12-04 11:51:45 +0900

    add permission enum

---
+++ Sources/Fileable/Permissions.swift
@@ -0,0 +1,14 @@
+//
+//  Permissions.swift
+//  Fileable
+//
+//  Created by Shota Shimazu on 2018/12/04.
+//
+
+import Foundation
+
+
+public enum FileablePermissions {
+    case Readable
+    case Writeable
+}
