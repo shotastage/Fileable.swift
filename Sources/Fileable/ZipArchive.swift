acece91dd9920069f0048abc3999e438738f11ee
Shota Shimazu <hornet.live.mf@gmail.com>
2018-12-09 23:37:20 +0900

    add compression level enum

--- Sources/Fileable/ZipArchive.swift
+++ Sources/Fileable/ZipArchive.swift
@@ -9,6 +9,33 @@ import Foundation
 import MiniZip
 
 
+
+
+public enum ZipCompressionLevel {
+    case NoCompression
+    case BestSpeed
+    case Default
+    case BestCompression
+    
+    
+    var minizipLevel: Int32 {
+        switch self {
+        case .NoCompression:
+            return Z_NO_COMPRESSION
+            
+        case .BestSpeed:
+            return Z_BEST_SPEED
+        
+        case .Default:
+            return Z_DEFAULT_COMPRESSION
+        
+        case .BestCompression:
+            return Z_BEST_COMPRESSION
+        }
+    }
+    
+}
+
 extension Path {
     
     func unzip(password: String? = nil) throws {
