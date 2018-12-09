eb97d7afe66fd2f86f008af8b8be287ed9d8d47f
Shota Shimazu <hornet.live.mf@gmail.com>
2018-12-09 17:39:08 +0900

    add zip archiver base

---
+++ Sources/Fileable/ZipArchive.swift
@@ -0,0 +1,21 @@
+//
+//  ZipArchive.swift
+//  Fileable
+//
+//  Created by Shota Shimazu on 2018/12/09.
+//
+
+import Foundation
+
+
+extension Path {
+    
+    func unzip() {
+        
+    }
+    
+    
+    func compress(password: String? = nil) {
+        ///
+    }
+}
