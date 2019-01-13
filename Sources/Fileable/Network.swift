76c1b21e98359d0695bff7f9f664da1c37215758
shotastage <hornet.live.mf@gmail.com>
2019-01-13 20:06:49 +0900

    add downloader

---
+++ Sources/Fileable/Network.swift
@@ -0,0 +1,21 @@
+//
+//  Network.swift
+//  Fileable
+//
+//  Created by Shota Shimazu on 2019/01/13.
+//  Copyright © 2017-2019 Shota Shimazu. All rights reserved.
+//
+
+import Foundation
+
+
+open class Downloader {
+    
+    public static let shared = Downloader()
+    
+    private init() { }
+    
+    func save(to: String) {
+        
+    }
+}
